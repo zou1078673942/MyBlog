8ea36e87dde92e7e70975a5de243f983403bfa1d
zou1078673942 <1078673942@qq.com>
2020-09-06 14:21:58 +0800

    blog

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -12,6 +12,25 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: 'root',
+      // database
+      database: 'react-blog',    
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1599111328947_9665';
 
